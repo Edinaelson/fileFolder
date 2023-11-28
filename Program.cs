62f8a8e765b9956852391a6818b99748b13ba273
Edinaelson <65689190+Edinaelson@users.noreply.github.com>
2023-11-28 16:20:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] diretorios = Directory.GetDirectories("c:\\");
+            
+            Console.WriteLine("Diretórios:");
+            foreach (string dir in diretorios)
+            {
+                Console.WriteLine(dir);
+            }
+        }
+    }
+}
